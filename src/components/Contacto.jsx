94f133186005dbe7582a4f174eba9bcadd6e2396
leobardo sanabria <leovoley2@gmail.com>
2024-08-11 15:28:51 -0500

    fe

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -4,31 +4,32 @@ import emailjs from '@emailjs/browser';
 const ContactFormReact = () => {
     const form = useRef();
   
+  
     const sendEmail = (e) => {
-      e.preventDefault();
-      emailjs
-        .sendForm(
-          process.env.service_fayk1hj,
-          process.env.template_5e9osht,
-          form.current,
-          process.env.ATNa9hCpUCnemBxjE
-        )
-        .then(
-          (result) => {
-            document.getElementById('result').innerHTML = 'Mensaje enviado con exito!';
-            console.log(result.text);
-          },
-          (error) => {
-            document.getElementById('result').innerHTML = 'Error al enviar su mensaje.';
-            console.log(error.text);
-          }
-        );
-    };
+        e.preventDefault();
+        emailjs
+          .sendForm(
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
+            form.current,
+            process.env.REACT_APP_USER_ID
+          )
+          .then(
+            (result) => {
+              document.getElementById('result').innerHTML = 'Mensaje enviado con éxito!';
+              console.log(result.text);
+            },
+            (error) => {
+              document.getElementById('result').innerHTML = 'Error al enviar su mensaje.';
+              console.log(error.text);
+            }
+          );
+      };
   
 
   return (
 
-    <form class="max-w-md mx-auto p-4 pt-6 md:p-6 lg:p-12" ref={form}>
+    <form class="max-w-md mx-auto p-4 pt-6 md:p-6 lg:p-12" ref={form} onSubmit={sendEmail}>
     <h1 class="text-3xl font-bold mb-4">Contactanos</h1>
     <label class="block mb-2">
       <span class="text-gray-700">Nombre:</span>
@@ -46,7 +47,7 @@ const ContactFormReact = () => {
       <span class="text-gray-700">Mensaje:</span>
       <textarea name="message" class="block w-full p-2 pl-10 text-sm text-gray-700" required></textarea>
     </label>
-    <button class="bg-blue-700 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" type="submit" onClick={sendEmail}>Enviar</button>
+    <button class="bg-blue-700 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" type="submit">Enviar</button>
     <div class="text-sm text-gray-600 mt-2" id="result"></div>
   </form>
   );
