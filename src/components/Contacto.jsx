29566ba6161c56b3734bae84a655323cb2ecbb05
leobardo sanabria <leovoley2@gmail.com>
2024-08-11 14:13:02 -0500

    prueba

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -32,30 +32,30 @@ import emailjs from '@emailjs/browser';
             <p className="text-3xl font-bold leading-7 text-center">Contacto</p>
             <form onSubmit={sendEmail} ref={form}  action="">
                 <div className="md:flex items-center mt-12">
-                    <div className="w-full md:w-1/2 flex flex-col">
+                    <fieldset className="w-full md:w-1/2 flex flex-col">
                         <label className="font-semibold leading-none">Nombre</label>
                         <input id='name' name='user_name' required type="text" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
-                    </div>
-                    <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    </fieldset>
+                    <fieldset class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
                         <label className="font-semibold leading-none">Asunto</label>
-                        <input id='phone' name='subject'  type="numeric" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
-                    </div>
+                        <input id='phone' name='subject'  type="text" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                    </fieldset>
                 </div>
                 <div class="md:flex items-center mt-8">
-                    <div className="w-full flex flex-col">
+                    <fieldset className="w-full flex flex-col">
                         <label className="font-semibold leading-none">Correo</label>
-                        <input id='email' name='user_email' required type="email" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
-                    </div>
+                        <input name='user_email' required type="email" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                    </fieldset>
                     
                 </div>
                 <div>
-                    <div className="w-full flex flex-col mt-8">
+                    <fieldset className="w-full flex flex-col mt-8">
                         <label className="font-semibold leading-none">Mensaje</label>
                         <textarea name='message' maxLength={500} required id='message' className="h-40 text-base leading-none text-black p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
-                    </div>
+                    </fieldset>
                 </div>
                 <div className="flex items-center justify-center w-full">
-                    <button className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                    <button type='submit' className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
                         Enviar mensaje
                     </button>
                 </div>
