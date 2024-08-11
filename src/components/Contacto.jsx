66fc5da03290b8a94bec211775e48588daad1102
leobardo sanabria <leovoley2@gmail.com>
2024-08-11 12:41:54 -0500

    cambios en el formulario

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -21,37 +21,37 @@ export const ContactUs = () => {
 
   return (
 
-<div class="w-full color3">
-    <div class=" h-96"></div>
-    <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
-        <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
-            <p class="text-3xl font-bold leading-7 text-center">Contacto</p>
+<div className="w-full color3">
+    <div className=" h-96"></div>
+    <div className="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+        <div className="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+            <p className="text-3xl font-bold leading-7 text-center">Contacto</p>
             <form onSubmit={handleSubmit} ref={refForm}  action="">
-                <div class="md:flex items-center mt-12">
-                    <div class="w-full md:w-1/2 flex flex-col">
-                        <label class="font-semibold leading-none">Nombre</label>
-                        <input id='name' name='name' required type="text" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                <div className="md:flex items-center mt-12">
+                    <div className="w-full md:w-1/2 flex flex-col">
+                        <label className="font-semibold leading-none">Nombre</label>
+                        <input id='name' name='name' required type="text" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
                     </div>
                     <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label class="font-semibold leading-none">telefono</label>
-                        <input id='phone' name='phone'  type="numeric" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                        <label className="font-semibold leading-none">telefono</label>
+                        <input id='phone' name='phone'  type="numeric" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
                     </div>
                 </div>
                 <div class="md:flex items-center mt-8">
-                    <div class="w-full flex flex-col">
-                        <label class="font-semibold leading-none">Correo</label>
-                        <input id='email' name='email' required type="email" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                    <div className="w-full flex flex-col">
+                        <label className="font-semibold leading-none">Correo</label>
+                        <input id='email' name='email' required type="email" className="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
                     </div>
                     
                 </div>
                 <div>
-                    <div class="w-full flex flex-col mt-8">
-                        <label class="font-semibold leading-none">Mensaje</label>
-                        <textarea name='message' maxLength={500} required id='message' class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
+                    <div className="w-full flex flex-col mt-8">
+                        <label className="font-semibold leading-none">Mensaje</label>
+                        <textarea name='message' maxLength={500} required id='message' className="h-40 text-base leading-none text-black p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
                     </div>
                 </div>
-                <div class="flex items-center justify-center w-full">
-                    <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                <div className="flex items-center justify-center w-full">
+                    <button className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
                         Enviar mensaje
                     </button>
                 </div>
